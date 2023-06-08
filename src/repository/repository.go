f12c08e14ea9b7a20f71c07eac29ef6ce42c047a
Iheanacho Ebere <eiheanacho52@gmail.com>
2023-06-08 22:33:16 +0100

    Feat: Update Interface

--- src/repository/repository.go
+++ src/repository/repository.go
@@ -9,12 +9,10 @@ type Repository interface {
 	CreateUser(user *models.User) error
 	GetCurrentUser(userID int) (*models.User, error)
 	UpdateUserBalance(userID int, amount int, transactionType models.TransactionType) (int, error)
-
 	CreateGame(game *models.Game) error
 	GetActiveGame() (*models.Game, error)
 	GetGamesByStatus(status models.GameStatus) ([]*models.Game, error)
 	UpdateGame(game *models.Game) error
-
 	CreateTransaction(newTransaction *models.Transaction) error
 	GetTransactions() ([]*models.Transaction, error)
 }
